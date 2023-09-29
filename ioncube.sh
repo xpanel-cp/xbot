50724f722dc3b0e779a7d43f38fb9a82257f9fb6
XPanel <127631174+xpanel-cp@users.noreply.github.com>
2023-09-29 15:10:09 +0330

    Update ioncube.sh

--- ioncube.sh
+++ ioncube.sh
@@ -7,12 +7,5 @@ echo "zend_extension = /usr/local/ioncube/ioncube_loader_lin_${PHPVERSION}so" > 
 sed -i 's@zend_extension = /usr/local/ioncube/ioncube_loader_lin_${PHPVERSION::-1}.so@@' /etc/php/${PHPVERSION::-1}/cli/php.ini
 echo "zend_extension = /usr/local/ioncube/ioncube_loader_lin_${PHPVERSION}so" >> /etc/php/${PHPVERSION::-1}/cli/php.ini
 systemctl restart apache2
-elif command -v yum >/dev/null; then
-wget -4 https://downloads.ioncube.com/loader_downloads/ioncube_loaders_lin_x86-64.tar.gz
-sudo tar xzf ioncube_loaders_lin_x86-64.tar.gz -C /usr/lib64/php/modules
-PHPVERSION=$(php -r 'echo PHP_VERSION;')
-echo "zend_extension = /usr/lib64/php/modules/ioncube/ioncube_loader_lin_${PHPVERSION::-3}.so" > /etc/php.d/00-ioncube.ini
-sed -i 's@zend_extension = /usr/lib64/php/modules/ioncube/ioncube_loader_lin_${PHPVERSION::-3}.so@@' /etc/php/${PHPVERSION::-3}/cli/php.ini
-echo "zend_extension = /usr/lib64/php/modules/ioncube/ioncube_loader_lin_${PHPVERSION::-3}.so" >> /etc/php/${PHPVERSION::-3}/cli/php.ini
 systemctl restart httpd
 
