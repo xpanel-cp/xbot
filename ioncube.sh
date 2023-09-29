2f68683b6bcc4a465491c2f1aa81a7e435a6cab5
XPanel <127631174+xpanel-cp@users.noreply.github.com>
2023-09-29 15:09:11 +0330

    Update ioncube.sh

--- ioncube.sh
+++ ioncube.sh
@@ -1,5 +1,4 @@
 
-if command -v apt-get >/dev/null; then
 wget -4 https://downloads.ioncube.com/loader_downloads/ioncube_loaders_lin_x86-64.tar.gz
 sudo tar xzf ioncube_loaders_lin_x86-64.tar.gz -C /usr/local
 sudo rm -rf ioncube_loaders_lin_x86-64.tar.gz
@@ -16,16 +15,4 @@ echo "zend_extension = /usr/lib64/php/modules/ioncube/ioncube_loader_lin_${PHPVE
 sed -i 's@zend_extension = /usr/lib64/php/modules/ioncube/ioncube_loader_lin_${PHPVERSION::-3}.so@@' /etc/php/${PHPVERSION::-3}/cli/php.ini
 echo "zend_extension = /usr/lib64/php/modules/ioncube/ioncube_loader_lin_${PHPVERSION::-3}.so" >> /etc/php/${PHPVERSION::-3}/cli/php.ini
 systemctl restart httpd
-fi
 
-
-if [ $(uname -m) = "aarch64" ]; then
-    arch="arm64"
-    wget -4 https://downloads.ioncube.com/loader_downloads/ioncube_loaders_lin_aarch64.tar.gz
-    sudo tar xzf ioncube_loaders_lin_aarch64.tar.gz -C /usr/lib/php/20210902
-sudo rm -rf ioncube_loaders_lin_aarch64.tar.gz
-PHPVERSION=$(php -i | grep /.+/php.ini -oE | sed 's/[^0-9.]*//g')
-echo "zend_extension = /usr/lib/php/20210902/ioncube/ioncube_loader_lin_${PHPVERSION}so" > /etc/php/${PHPVERSION::-1}/apache2/conf.d/00-ioncube.ini
-echo "zend_extension = /usr/lib/php/20210902/ioncube/ioncube_loader_lin_${PHPVERSION}so" >> /etc/php/${PHPVERSION::-1}/cli/php.ini
-systemctl restart apache2
-fi
